ccb68145fa1ababaa70de145dad68eec318611c3
Nicholas Canzoneri <canzoneri.nicholas@gmail.com>
2013-06-03 21:55:07 -0400

    recipe for bare bones development

---
+++ recipes/default.rb
@@ -0,0 +1,30 @@
+bash "update resources" do
+  code <<-EOH
+  apt-get update
+  EOH
+end
+
+%w[git vim emacs ruby].each do |pkg|
+  package pkg do
+    action :install
+  end
+end
+
+git "/etc/dotfiles" do
+  repository "git://github.com/nickcanz/dotfiles.git"
+  reference "master"
+  action :sync
+end
+
+%w[rake bundler].each do |gem|
+  gem_package gem do
+    action :install
+  end
+end
+
+bash "install dotfiles" do
+  cwd "/etc/dotfiles"
+  code <<-EOH
+  rake install
+  EOH
+end
